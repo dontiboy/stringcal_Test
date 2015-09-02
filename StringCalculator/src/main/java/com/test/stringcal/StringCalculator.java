15a8eac462b6e7b1b5b81702493b7fbe7d2a21dd
Srinivasa <dontiboy@gmail.com>
2015-09-02 23:16:51 +0100

    StringCalculator - Reg4 - refactored code

--- StringCalculator/src/main/java/com/test/stringcal/StringCalculator.java
+++ StringCalculator/src/main/java/com/test/stringcal/StringCalculator.java
@@ -4,23 +4,41 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    private String DELIMITER = ",|\n";
+
+    public static final int REGEX_GROUP_NUMBER_STRING = 2;
+    public static final int REGEX_NUMBER_OF_USER_DEFINED_DELIMITERS = 1;
+    public static final String PREDEFINED_REGEX = ",|\n";
+    public static final String _USER_DEFINED_REGEX_PATTERN = "^//(.)\n(.*)";
 
     public int add(String str) {
-        Matcher matcher = Pattern.compile("^//(.)\n(.*)").matcher(str);
-        String numberString = str;
-        if (matcher.find()) {
-            DELIMITER = matcher.group(1);
-            numberString = matcher.group(2);
-        }
-        return calculateSum(getNumbers(numberString));
+
+        Matcher matcher = Pattern.compile(_USER_DEFINED_REGEX_PATTERN).matcher(str);
+        String delimiters = getDelimiters(matcher);
+        String numberString = getNumberString(str, matcher.reset());
+        return calculateSum(getNumbers(numberString, delimiters));
     }
 
-    private String[] getNumbers(String str) {
+    private String getNumberString(String str, Matcher matcher) {
+        if (matcher.find()) {
+            return matcher.group(REGEX_GROUP_NUMBER_STRING);
+        } else {
+            return str;
+        }
+    }
+
+    private String getDelimiters(Matcher matcher) {
+        if (matcher.find()) {
+            return matcher.group(REGEX_NUMBER_OF_USER_DEFINED_DELIMITERS);
+        }else{
+            return PREDEFINED_REGEX;
+        }
+    }
+
+    private String[] getNumbers(String str, String delimiter) {
         if (str.isEmpty()) {
             return new String[0];
         } else {
-            return str.split(DELIMITER);
+            return str.split(delimiter);
         }
     }
 
